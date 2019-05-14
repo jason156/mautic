05f11af8099abd7538a364cbe8411a5c0d8783ac
John Linhart <admin@escope.cz>
2019-05-14 17:20:50 +0200

    Add email send event to the webhooks

--- app/bundles/EmailBundle/EventListener/WebhookSubscriber.php
+++ app/bundles/EmailBundle/EventListener/WebhookSubscriber.php
@@ -17,10 +17,8 @@ use Mautic\EmailBundle\Event\EmailOpenEvent;
 use Mautic\WebhookBundle\Event\WebhookBuilderEvent;
 use Mautic\WebhookBundle\EventListener\WebhookModelTrait;
 use Mautic\WebhookBundle\WebhookEvents;
+use Mautic\EmailBundle\Event\EmailSendEvent;
 
-/**
- * Class WebhookSubscriber.
- */
 class WebhookSubscriber extends CommonSubscriber
 {
     use WebhookModelTrait;
@@ -31,6 +29,7 @@ class WebhookSubscriber extends CommonSubscriber
     public static function getSubscribedEvents()
     {
         return [
+            EmailEvents::EMAIL_ON_SEND      => ['onEmailSend', 0],
             EmailEvents::EMAIL_ON_OPEN      => ['onEmailOpen', 0],
             WebhookEvents::WEBHOOK_ON_BUILD => ['onWebhookBuild', 0],
         ];
@@ -44,13 +43,44 @@ class WebhookSubscriber extends CommonSubscriber
     public function onWebhookBuild(WebhookBuilderEvent $event)
     {
         // add checkbox to the webhook form for new leads
+        $mailSend= [
+            'label'       => 'mautic.email.webhook.event.send',
+            'description' => 'mautic.email.webhook.event.send_desc',
+        ];
         $mailOpen = [
             'label'       => 'mautic.email.webhook.event.open',
             'description' => 'mautic.email.webhook.event.open_desc',
         ];
 
         // add it to the list
+        $event->addEvent(EmailEvents::EMAIL_ON_SEND, $mailSend);
         $event->addEvent(EmailEvents::EMAIL_ON_OPEN, $mailOpen);
+    }
+
+    /**
+     * @param EmailSendEvent $event
+     */
+    public function onEmailSend(EmailSendEvent $event)
+    {
+        // Ignore test email sends.
+        if ($event->isInternalSend()) {
+            return;
+        }
+
+        $this->webhookModel->queueWebhooksByType(
+            EmailEvents::EMAIL_ON_SEND,
+            [
+                'email'       => $event->getEmail(),
+                'contact'     => $event->getLead(),
+                'tokens'      => $event->getTokens(),
+                'contentHash' => $event->getContentHash(),
+                'idHash'      => $event->getIdHash(),
+                'content'     => $event->getContent(),
+                'subject'     => $event->getSubject(),
+                'source'      => $event->getSource(),
+                'headers'     => $event->getTextHeaders(),
+            ]
+        );
     }
 
     /**
